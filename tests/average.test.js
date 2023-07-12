39c68aad1db84b456a4d5cecfa4b33d3518d3fde
Jimi Hunter <jimi.hunter008@gmail.com>
2023-07-12 15:52:53 +0100

    ultimate CI/CD project

--- tests/average.test.js
+++ tests/average.test.js
@@ -1,20 +1,20 @@
 const config = require('../utils/config');
 
-// const { average } = require('./for_testing');
+const { average } = require('../utils/for_testing');
 
-// describe('average', () => {
-//   test('of one value is the value itself', () => {
-//     expect(average([1])).toBe(1);
-//   });
+describe('average', () => {
+  test('of one value is the value itself', () => {
+    expect(average([1])).toBe(1);
+  });
 
-//   test('of many is calculated right', () => {
-//     expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5);
-//   });
+  test('of many is calculated right', () => {
+    expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5);
+  });
 
-//   test('of empty array is zero', () => {
-//     expect(average([])).toBe(0);
-//   });
-// });
+  test('of empty array is zero', () => {
+    expect(average([])).toBe(0);
+  });
+});
 
 console.log(config.JENKINS_TEST);
 console.log(config.COMMENT);
