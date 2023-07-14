aa8b5856c656b9a9ab64552f23035a5c739d903b
Jimi Hunter <jimi.hunter008@gmail.com>
2023-07-14 01:06:49 +0100

    new test

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
+console.log(process.env.SONAR_TOKEN);
 const JENKINS_TEST = process.env.JENKINS_TEST;
 const COMMENT = process.env.COMMENT;
-console.log(process.env.SONAR_TOKEN);
 
 module.exports = {
   JENKINS_TEST,
